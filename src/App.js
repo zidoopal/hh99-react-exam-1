d2ebefecdf6b08b40320631a24bbf5ee5bac6c10
zidoopal <yuzzzin@gmail.com>
2023-08-31 10:09:18 +0900

    input 입력 / Todo 추가 기능구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div></div>;
+  // input
+  const [title, setTitle] = useState('');
+
+  // TodoList
+  const [todos, setTodos] = useState([
+    {
+      title: 'react를 배워봅시다',
+    },
+  ]);
+
+  // input 입력값 Todos에 반영
+  const onChangeContent = (e) => setTitle(e.target.value);
+
+  // TodoList에 입력 값 추가
+  const addTodoHandler = () => {
+    const newTodo = {
+      id: todos[todos.length - 1] + 1,
+      title,
+    };
+
+    setTodos([...todos, newTodo]);
+    setTitle('');
+  };
+
+  return (
+    <div className="App">
+      <div className="input-group">
+        <input
+          type="text"
+          name="title"
+          className="input-body"
+          value={title}
+          onChange={onChangeContent}
+        />
+        <button type="submit" onClick={addTodoHandler}>
+          추가하기
+        </button>
+      </div>
+      <h1 className="title">Todo List</h1>
+      <div className="todo-container">
+        {todos.map((todo) => (
+          <div className="todo-item" key={todo.id}>
+            {todo.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
